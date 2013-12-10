1d92e9c08a34d286d9d9eb23dc19ad40d2bf02dd
Elliot <ElliotLedley@gmail.com>
2013-12-10 09:30:10 -0700

    Update dbcon.php

--- 2Shoes/2s-code/db/dbcon.php
+++ 2Shoes/2s-code/db/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysql_connect("Twoshoes.db.11412629.hostedresource.com", "Twoshoes", "L3tsdothis!") or die ("Can't connect to database.");
+$con = mysql_connect("Twoshoes.db.11412629.hostedresource.com", "Twoshoes", "Feild123for!") or die ("Can't connect to database.");
 mysql_select_db("Twoshoes")or die ("Can't connect to database.");
 
 //https://p3nlmysqladm002.secureserver.net/dgrid50/209/index.php?lang=en-utf-8&token=5dc5de5c18a6d755e6de005b1ba2da4e
